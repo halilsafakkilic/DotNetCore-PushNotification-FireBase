c476989f20a5921adb9749059328cbfbf7f0e0cb
halilsafakkilic <root@poisnet.net>
2020-05-24 13:47:43 +0300

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.IO;
+using FirebaseAdmin;
+using FirebaseAdmin.Messaging;
+using Google.Apis.Auth.OAuth2;
+
+namespace FCM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FirebaseApp.Create(new AppOptions()
+            {
+                Credential = GoogleCredential.FromFile(Directory.GetCurrentDirectory() + "/serviceAccountKey.json"),
+            });
+
+            Start();
+        }
+
+        static void Start()
+        {
+            Console.Write("Please enter target Token: ");
+
+            var messageInput = Console.ReadLine();
+            if (String.IsNullOrEmpty(messageInput))
+            {
+                Console.WriteLine("Are u disco or cola?"); // Only those who watch "Cem Yilmaz" can understand. :)
+
+                Start();
+            }
+
+            SendNotification(messageInput);
+
+            Start();
+        }
+
+        static void SendNotification(string token)
+        {
+            var message = new Message
+            {
+                Token = token,
+                Notification = new Notification
+                {
+                    Title = "Firebase Notification Test",
+                    Body = "Hi, everyone! Time: " + DateTime.Now.ToString()
+                }
+            };
+
+            var result = FirebaseMessaging.DefaultInstance.SendAsync(message)
+                .ConfigureAwait(false)
+                .GetAwaiter().GetResult();
+
+            Console.WriteLine("Response: " + result);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,5 @@
+# Push Notification Sample for Firebase Cloud Messaging
+
+First you need a client (see: https://github.com/firebase/quickstart-js/tree/master/messaging). If ready your client side; go to Firebase Console > Settings > Service Accounts and then download Firebase Admin SDK credential. This downloaded *.json file should be replace name it serviceAccountKey.json and this file move in this project root folder. Anymore we need a user token for push notification.
+
+*Enjoy it now :)*
